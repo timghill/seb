3927c7cb499f1b7f098c0b0656522ebd382364e7
tghill <tghill@uwaterloo.ca>
2020-12-15 14:14:45 -0500

    Update driver

--- examples/kaskawulsh/driver.m
+++ examples/kaskawulsh/driver.m
@@ -20,8 +20,8 @@ sim.params.tout=12;                % Time interval between saving model outputs 
 
 sim.params.Tice=-3;         % 12 m ice temperature (C)
 
-sim.z0=0.003;               % Stability length (3 mm)
-sim.z=1.35;                 % Height of temperature, RH measurements above the glacier surface (m)
+sim.params.z0=0.003;               % Stability length (3 mm)
+sim.params.z=1.35;                 % Height of temperature, RH measurements above the glacier surface (m)
 sim.params.T_elev=1154;     % Elevation of temperature measurements. Used to spatially distribute T (m)
 sim.params.T_lapse_rate=3.9770e-3;  % Temp lapse rate (C.m-1)
 
